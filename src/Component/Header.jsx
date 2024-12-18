d9e6314e389867997e060a6789b68d18ddaac291
Ahmed <ml967799@gmail.com>
2024-12-18 21:56:00 +0100

    responsive

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,23 +1,4 @@
-/*
-import userImage from '../assets/user.png';
 
-export default function Header() {
-  return (
-    <div className='header'>
-      <div className='titre'>Prototype</div>
-      <div className='user'>
-        <img src={userImage} alt='user name' />
-        <div className='userName'>
-          <div>
-            <b>Hello, </b>Ahmed 
-          </div>
-          <div className='date'>24-12-2024</div>
-        </div>
-      </div>
-    </div>
-  );
-}
-*/
 import userImage from '../assets/user.png';
 import '../Styles/Header.css'; // Add a dedicated CSS file for styling
 
